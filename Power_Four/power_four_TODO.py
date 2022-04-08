bdd30f47c6615c081c997e17425e0408048477dc
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-04-08 14:58:14 +0200

    Start cleaning mess

--- Power_Four/power_four_TODO.py
+++
@@ -1,3 +0,0 @@
-# Todo : - make entries for names of player and for statistics
-# Todo : - make a file/dictionary which keep in memory the results and the stats of the players
-# Todo : and of course : make the computer able to play.
