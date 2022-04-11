411e4fd80fe753c2c9cc53fdce487a18320932be
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-04-11 22:57:22 +0200

    Traduce and comment parts of code

--- try_add_circle.py
+++ try_add_circle.py
@@ -1,29 +1,32 @@
 from tkinter import *
 
-racine = Tk()
-zone_dessin = Canvas(racine, width=500, height=500)
-zone_dessin.pack()
-zone_dessin.create_line(0, 0, 500, 500)
-zone_dessin.create_rectangle(490, 200, 500, 100)
-zone_dessin.create_text(100, 100, text="hello")
+root = Tk()
+drawing_zone = Canvas(root, width=500, height=500)
+drawing_zone.pack()
+drawing_zone.create_line(0, 0, 500, 500)
+drawing_zone.create_rectangle(490, 200, 500, 100)
+drawing_zone.create_text(100, 100, text="hello")
+
+# Oval inscribed in the rectangle whose x1, y1, x2, y2 are given
+drawing_zone.create_oval(400, 400, 300, 300, outline="gray",
+                         width=1, fill="#%02x%02x%02x" % (255, 3, 77))
+# Example of drawing a polygon: the coordinates of the points must be given
+drawing_zone.create_polygon(100, 200, 200, 200, 250, 290, 200, 380, 100, 380, 50, 290,
+                            outline="red", width=2, fill="yellow")
 
 
-# zone_dessin.create_oval(100, 100, 200, 200, outline ="grey",
-#                                      width =1, fill =[255,3,77])
-# zone_dessin.create_polygon(100,100,200,100,250,190,200,280,100,280,50,190, outline="red", width=2, fill= "yellow")
-
-
-def qzuit(event):
+def quit_(event):
+    """Destroy the canvas and the 'root', if the top arrow is pressed"""
     if event.keysym == "Up":
-        zone_dessin.destroy()
-        racine.destroy()
+        drawing_zone.destroy()
+        root.destroy()
 
 
-bouton_sortir = Button(racine, text="Sortir", command=racine.destroy)
-bouton_sortir.pack()
+exit_button = Button(root, text="Exit", command=root.destroy)
+exit_button.pack()
 
-zone_dessin.focus_set()
-zone_dessin.bind("<Up>", qzuit)
-zone_dessin.pack()
+drawing_zone.focus_set()
+drawing_zone.bind("<Up>", quit_)
+drawing_zone.pack()
 
-racine.mainloop()
+root.mainloop()
