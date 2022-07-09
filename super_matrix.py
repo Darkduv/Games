90072cbde22c4af05aa45ed0d43a2ff074d41535
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-07-09 09:56:53 +0200

    refactoring, pylint ok

--- super_matrix.py
+++ super_matrix.py
@@ -1,11 +1,14 @@
+"""Defines special matrix and lists to use in games"""
 from random import randint
 
 
 class SuperMatrix(list):
+    """Matrix inheriting from list"""
+
     def __init__(self, number=0, n_lig=12, n_col=12, fill=True):
         list.__init__([])
         if fill:
-            for i in range(n_lig):
+            for _ in range(n_lig):
                 self.append([number] * n_col)
 
     def copy(self):
@@ -23,11 +26,14 @@ class SuperMatrix(list):
 
 
 class SuperList(list):
+    """Regular list with a twist:
 
-    def __setitem__(self, key, value):
+    can append a value if we try to set a non-existant index"""
+
+    def __setitem__(self, index, value):
         try:
-            assert isinstance(key, int)
-            list.__setitem__(self, key, value)
+            assert isinstance(index, int)
+            list.__setitem__(self, index, value)
         except IndexError:
             self.append(value)
 
@@ -37,34 +43,37 @@ class ListWeight(list):
     """List of each position available with its weight :
     the list is in the form of [[position, weight],...]"""
 
-    def position_weight_max(self):
-        if self:  # list not empty
-            maxi = self[0][1]
-            l_max = [0]
-            for i in range(len(self)):
-                if self[i][1] > maxi:
-                    maxi = self[i][1]
-                    l_max = [i]
-                elif self[i][1] == maxi:
-                    l_max.append(i)
-            l = len(l_max)
-            i_max = l_max[randint(0, l-1)]
-            return self[i_max]
-        else:
+    def _position_weight_min_max(self, keep_first):
+        """Returns randomly a position with the highest (or lowest) weight.
+
+        Returns [] if list of positions empty.
+        keep_first(a, b) returns True if we want to keep the first value """
+        if not self:  # list empty
             return []
+        extreme = self[0][1]
+        l_extreme = [0]
+        for [pos, weight] in self:
+            if keep_first(weight, extreme):
+                extreme = weight
+                l_extreme = [[pos, weight]]
+            elif weight == l_extreme:
+                l_extreme.append([pos, weight])
+        length = len(l_extreme)
+        pos_weight = l_extreme[randint(0, length-1)]
+        return pos_weight
+
+    def position_weight_max(self):
+        """Returns randomly a position with the highest weight.
+
+        Returns [] if list of positions empty."""
+        def keep_first(weight, maxi):
+            return weight > maxi
+        return self._position_weight_min_max(keep_first)
 
     def position_weight_min(self):
-        if self:  # list not empty
-            mini = self[0][1]
-            l_min = [0]
-            for i in range(len(self)):
-                if self[i][1] < mini:
-                    mini = self[i][1]
-                    l_min = [i]
-                elif self[i][1] == mini:
-                    l_min.append(i)
-            l = len(l_min)
-            i_min = l_min[randint(0, l-1)]
-            return self[i_min]
-        else:
-            return []
+        """Returns randomly a position with the highest weight.
+
+        Returns [] if list of positions empty."""
+        def keep_first(weight, maxi):
+            return weight < maxi
+        return self._position_weight_min_max(keep_first)
