8904e129e1cec00e371ee6a123ba1fc53371f97b
Darkduv <40823426+Darkduv@users.noreply.github.com>
2022-07-11 01:34:12 +0200

    Big refactoring : creates Grid class

--- othello_optimized.py
+++ othello_optimized.py
@@ -1,20 +1,113 @@
 """
 ###########################################
 #  Game of othello/reversi                #
-#  References : None                      #
+#  References: None                       #
 #                                         #
 #                                         #
 #                                         #
 #  Version du 12/03/2016                  #
-#            - Licence : GPL              #
+#            - Licence: GPL               #
 ###########################################
 """
 
 import tkinter
+from typing import Tuple, TypeAlias
+import numpy as np
+
+Position: TypeAlias = Tuple[int, int]
+
+
+class InvalidPosition(ValueError):
+    """Custom error when accessing a wrong (row, col) position."""
+
+
+class Grid:
+    """Grid ot othello/reversi."""
+
+    all_directions = [[0, 1], [1, 1], [1, 0], [0, -1], [-1, -1],
+                      [-1, 0], [-1, 1], [1, -1]]
+
+    def __init__(self):
+        self.n_row, self.n_col = 8, 8
+        self.grid = np.zeros((self.n_row, self.n_col), dtype=int)
+
+    def init(self) -> None:
+        self.grid = np.zeros((self.n_row, self.n_col), dtype=int)
+
+        self.grid[3, 3] = 2
+        self.grid[4, 4] = 2
+        self.grid[3, 4] = 1
+        self.grid[4, 3] = 1
+
+    def nb_disks(self) -> dict[int, int]:
+        unique, counts = np.unique(self.grid, return_counts=True)
+
+        return dict(zip(unique, counts))
+
+    def __getitem__(self, item: Position):
+        return self.grid[item]
+
+    def __setitem__(self, key: Position, value: int):
+        self.grid[key] = value
+
+    def valid_position(self, row: int, col: int) -> bool:
+        """Is [row, col] in the grid?"""
+        return 0 <= row < self.n_row and 0 <= col < self.n_col
+
+    def valid_move_row_col(self, row: int, col: int, player: int) -> bool:
+        """Is [row, col] a valid move for `player`?
+
+        Searches over the eight (or less) possible directions"""
+        if self.grid[row, col] != 0:
+            return False  # place must be empty to be valid
+        for direction in self.all_directions:
+            if self.move_direction(direction, row, col, player):
+                return True
+        return False
+
+    def all_flips_move(self, row: int, col: int, player: int) -> list[Position]:
+        """Returns the list of disks flipped due to move [row, col]"""
+        all_flips = []
+        for direction in self.all_directions:
+            all_flips.extend(self.move_direction(direction, row, col, player))
+        return all_flips
+
+    def flips_list(self, list_position: list[Position], player: int) -> None:
+        # Todo : flips from a list: player should not be needed
+        for position in list_position:
+            self[position] = player
+
+    def verify(self, player: int) -> list[Tuple[int, int]]:
+        list_ok = []
+        for i in range(self.n_row):
+            for j in range(self.n_col):
+                if self.valid_move_row_col(i, j, player):
+                    list_ok.append((i, j))
+        return list_ok
+
+    def move_direction(self, direction, row: int, col: int,
+                       player: int) -> list[Position]:
+        """Test a move of `player` from row,col, in the given `direction`.
+
+        If `test == True` only tests the move. If False, makes it."""
+        di, dj = direction
+        i = row + di
+        j = col + dj
+        l_flip_pos = []
+        while self.valid_position(i, j) and\
+                self.grid[i, j] not in [player, 0]:
+            l_flip_pos.append((i, j))
+            i += di
+            j += dj
+        if not l_flip_pos \
+                or not self.valid_position(i, j) \
+                or self.grid[i, j] != player:
+            return []
+        return l_flip_pos
 
 
 class MenuBar(tkinter.Frame):
-    """bar of menu rolling"""
+    """Bar of dropdown menus"""
 
     def __init__(self, boss=None):
         super().__init__(borderwidth=2, relief=tkinter.GROOVE)
@@ -22,8 +115,6 @@ class MenuBar(tkinter.Frame):
         file_menu = tkinter.Menubutton(self, text='File')
         file_menu.pack(side=tkinter.LEFT, padx=5)
         me1 = tkinter.Menu(file_menu)
-        me1.add_command(label='Options', underline=0,
-                        command=boss.options)
         me1.add_command(label='Restart', underline=0,
                         command=boss.reset)
         me1.add_command(label='Quit', underline=0,
@@ -36,13 +127,16 @@ class MenuBar(tkinter.Frame):
         me1 = tkinter.Menu(help_menu)
         me1.add_command(label='Principle of the game', underline=0,
                         command=boss.principle)
-        me1.add_command(label='By the way ...', underline=0,
-                        command=boss.by_the_way)
+        me1.add_command(label='About', underline=0,
+                        command=boss.about)
         help_menu.configure(menu=me1)
 
 
 class Panel(tkinter.Frame):
-    """Panel de jeu (grille de n x m cases)"""
+    """Game panel: game with graphics"""
+
+    _colors = {1: 'Black', 2: 'White'}
+    _board_color = "dark olive green"
 
     def __init__(self):
         # The panel of game is constituted of a re-scaling grid
@@ -52,175 +146,168 @@ class Panel(tkinter.Frame):
         # canvas are adapted in consequence.
         super().__init__()
         self.n_row, self.n_col = 8, 8  # initial grid = 4 x 4
-        # Link of the event <resize> with an adapted manager :
+        # Link of the event <resize> with an adapted manager:
         self.bind("<Configure>", self.rescale)
-        # Canvas :
-        self.can = tkinter.Canvas(self, bg="dark olive green", borderwidth=0,
+        # Canvas:
+        self.can = tkinter.Canvas(self, bg=self._board_color, borderwidth=0,
                                   highlightthickness=1,
                                   highlightbackground="white")
-        # Link of the event <click of the mouse> with its manager :
+        # Link of the event <click of the mouse> with its manager:
         self.can.bind("<Button-1>", self.click)
         self.can.pack(side=tkinter.LEFT)
         self.can_bis = tkinter.Label(text="Black's turn",
                                      font="Helvetica 25 normal")
-        self.can_bis.pack(side=tkinter.RIGHT)
-        self.state = []  # construction of a list of lists
-        self.width, self.height = 2, 2
-        self.cote = 0
-        self.player = 0
+        self.can_bis.pack(side=tkinter.RIGHT, padx=20)
+        self.grid = Grid()  # construction of a list of lists
+
+        self.player = 1
         self.place_ok = []
-        self.nb_pawn_player = [2, 2]
         self.init_jeu()
+
+    @classmethod
+    def color_player(cls, player: int) -> str:
+        return cls._colors[player]
+
+    @classmethod
+    def disk_color(cls, player: int) -> str:
+        if player == 0:
+            return cls._board_color
+        return cls.color_player(player).lower()
+
+    def plot_nb_pawn(self, msg="{player}'s turn."):
+        nb1, nb2 = self.nb_disks_players()
+        self.can_bis.configure(
+            text=f"{msg.format(player=self.color_player(self.player)):15}\n\n"
+                 f"{self.color_player(1)}: {nb1: 2}\n"
+                 f"{self.color_player(2)}: {nb2: 2}")
 
     def init_jeu(self):
         """Initialisation of the list which remember the state of the game"""
-        self.state = []
-        self.player = 0
-        for _ in range(12):  # equal to a panel
-            self.state.append([2] * 12)  # of 12 lines x 12 columns
+        self.grid.init()
+        self.player = 1
+        self.plot_nb_pawn()
 
-        self.state[self.n_row // 2 - 1][self.n_col // 2 - 1] = 0
-        self.state[self.n_row // 2][self.n_col // 2] = 0
-        self.state[self.n_row // 2 - 1][self.n_col // 2] = 1
-        self.state[self.n_row // 2][self.n_col // 2 - 1] = 1
-        self.nb_pawn_player = [2, 2]
-        self.can_bis.configure(
-            text=f"{['White', 'Black'][not self.player]}'s turn.\n\n"
-                 f"White: {self.nb_pawn_player[0]}\n"
-                 f"Black: {self.nb_pawn_player[1]}")
-
-    def rescale(self, event):
+    def rescale(self, _: tkinter.Event):
         """Operations made at each rescaling"""
-        # the properties which are linked to the event of reconfiguration
-        # contain all the new sizes of the panel :
-        self.width, self.height = event.width - 4, event.height - 4
-        # The subtraction of 4 pixels allowed to compensate the width
-        # of the `highlightbordure` rolling the canvas
         self.trace_grille()
+
+    @property
+    def square_side(self) -> int:
+        #  maximum possible width and height for the squares:
+        l_max = self.winfo_width() // self.n_col
+        h_max = self.winfo_height() // self.n_row
+        # the side of the square will be the smallest of these dimensions:
+        return min(l_max, h_max)
+
+    @property
+    def shape(self) -> Tuple[int, int]:
+        return self.square_side * self.n_col, self.square_side * self.n_row
+
+    def change_player(self):
+        self.player %= 2
+        self.player += 1
+
+    def nb_disks_players(self) -> Tuple[int, int]:
+        nb_disks = self.grid.nb_disks()
+        return nb_disks.get(1, 0), nb_disks.get(2, 0)
 
     def trace_grille(self):
         """Layout of the grid, in function of dimensions and options"""
-        # maximal width and height possibles for the cases :
-        l_max = self.width / self.n_col
-        h_max = self.height / self.n_row
-        # the side of a case would be the smallest of the two dimensions :
-        self.cote = min(l_max, h_max)
-        # -> establishment of new dimensions for the canvas :
-        wide, high = self.cote * self.n_col, self.cote * self.n_row
-        self.can.configure(width=wide, height=high)
+        # -> Configuration of the new canvas dimensions:
+        width, height = self.shape
+        self.can.configure(width=width, height=height)
         # Layout of the grid:
         self.can.delete(tkinter.ALL)  # erasing of the past Layouts
-        s = self.cote
+        square_size = self.square_side
+        s = square_size
         for _ in range(self.n_row - 1):  # horizontal lines
-            self.can.create_line(0, s, wide, s, fill="white")
-            s += self.cote
-        s = self.cote
+            self.can.create_line(0, s, width, s, fill="white")
+            s += square_size
+        s = square_size
         for c in range(self.n_col - 1):  # vertical lines
-            self.can.create_line(s, 0, s, high, fill="white")
-            s += self.cote
+            self.can.create_line(s, 0, s, height, fill="white")
+            s += square_size
         # Layout of all the pawns,
-        # white or black according to the sate of the game :
+        # white or black according to the state of the game:
         for r in range(self.n_row):
             for c in range(self.n_col):
-                x1 = c * self.cote + 3  # size of pawns =
-                x2 = (c + 1) * self.cote - 3  # size of the case -10
-                y1 = r * self.cote + 3  #
-                y2 = (r + 1) * self.cote - 3
-                color = ["white", "black", "dark olive green"][self.state[r][c]]
-                self.can.create_oval(x1, y1, x2, y2, outline="grey",
-                                     width=1, fill=color)
+                color = self.disk_color(self.grid[r, c])
+                self.draw_disk(r, c, color)
 
-    def roll(self, i, j):
-        """ return True if the case 'touch' a non-empty case, False else """
-        for x in range(i - 1, i + 2):
-            for y in range(j - 1, j + 2):
-                if 0 <= x < self.n_row and 0 <= y < self.n_col \
-                        and self.state[x][y] != 2:
-                    return True
-        return False
+    def draw_disk(self, row: int, col: int, color: str):
+        square_size = self.square_side
+        self.can.create_oval(col * square_size + 3,
+                             row * square_size + 3,
+                             (col + 1) * square_size - 3,
+                             (row + 1) * square_size - 3,
+                             outline="grey", width=1, fill=color)
 
-    def verify(self):
-        list_ok = []
-        self.player += 1
-        self.player %= 2
-        for i in range(self.n_row):
-            for j in range(self.n_col):
-                if self.state[i][j] == 2 and self.roll(i, j):
-                    for direction in [[0, 1], [1, 1], [1, 0], [0, -1],
-                                      [-1, -1], [-1, 0], [-1, 1], [1, -1]]:
-                        if self.movement_direction(direction, i, j):
-                            list_ok.append([i, j])
-        self.player += 1
-        self.player %= 2
-        self.place_ok = list_ok
+    def make_flips(self, list_flips: list[Position], player: int) -> None:
+        """Make the flips of the move [row, col]  of `player`.
 
-    def movement_direction(self, direction, row, col, test=True):
-        i = row + direction[0]
-        j = col + direction[1]
-        nb = 0
-        while 0 <= i < self.n_row and 0 <= j < self.n_col and\
-                self.state[i][j] is not self.player and 2 != self.state[i][j]:
-            i += direction[0]
-            j += direction[1]
-            nb += 1
-        if nb > 0 and i != self.n_row and j != self.n_col and \
-                self.state[i][j] == self.player:
-            if test:
-                return True
-            for k in range(1, nb + 1):
-                self.state[row + k * direction[0]][
-                    col + k * direction[1]] = self.player
-                self.nb_pawn_player[self.player] += 1
-                self.nb_pawn_player[not self.player] -= 1
-                color = ["white", "black", "dark olive green"][self.player]
-                self.can.create_oval((col + k * direction[1]) * self.cote + 3,
-                                     (row + k * direction[0]) * self.cote + 3,
-                                     (col + k*direction[1] + 1) * self.cote - 3,
-                                     (row + k*direction[0] + 1) * self.cote - 3,
-                                     outline="grey", width=1, fill=color)
-                self.update()
-                self.can.after(200)
-        return False
+        If `test == True` only tests the move. If False, makes it."""
 
-    def click(self, event):
-        """Management of the mouse click : return the pawns"""
-        # We start to determinate the line and the columns :
-        row, col = int(event.y / self.cote), int(event.x / self.cote)
-        if not self.place_ok:
-            self.verify()
-
-        if not self.place_ok:
-            self.player += 1
-            self.player %= 2
-            self.verify()
-
-        if 0 <= row < self.n_row and 0 <= col < self.n_col and \
-                [row, col] in self.place_ok:
-            self.place_ok = []
-            self.player += 1
-            self.player %= 2
-            self.state[row][col] = self.player
-            self.nb_pawn_player[self.player] += 1
-            color = ["white", "black", "dark olive green"][self.player]
-            self.can.create_oval(col * self.cote + 3, row * self.cote + 3,
-                                 (col + 1) * self.cote - 3,
-                                 (row + 1) * self.cote - 3,
-                                 outline="grey", width=1, fill=color)
+        self.grid.flips_list(list_flips, player)
+        color = self.disk_color(player)
+        for i, j in list_flips:
+            self.draw_disk(i, j, color)
             self.update()
             self.can.after(200)
-            for direction in [[0, 1], [1, 1], [1, 0], [0, -1], [-1, -1],
-                              [-1, 0], [-1, 1], [1, -1]]:
-                self.movement_direction(direction, row, col, test=False)
 
-            self.can_bis.configure(
-                text=f"{['White', 'Black'][not self.player]}'s turn.\n\n"
-                     f"White: {self.nb_pawn_player[0]}\n"
-                     f"Black: {self.nb_pawn_player[1]}")
+    def position_click(self, event: tkinter.Event) -> Position:
+        """Determines the row and the column of the grid where is the click:"""
+        square_size = self.square_side
+        return int(event.y / square_size), int(event.x / square_size)
+
+    def click(self, event: tkinter.Event):
+        """Management of the mouse click: return the pawns"""
+        row, col = self.position_click(event)
+        if not self.grid.valid_position(row, col):
+            return  # invalid move
+        list_ok = self.grid.verify(self.player)
+
+        if not list_ok:  # empty list
+            self.end()
+            return
+
+        if (row, col) not in list_ok:
+            return
+
+        self.grid[row, col] = self.player
+
+        # animation :
+        color = self.disk_color(self.player)
+        self.draw_disk(row, col, color)
+        self.update()
+        self.can.after(200)
+        l_flips = self.grid.all_flips_move(row, col, self.player)
+        self.make_flips(l_flips, self.player)
+        # end of the animation
+
+        self.change_player()
+        list_ok = self.grid.verify(self.player)
+        if not list_ok:  # other player can not play
+            self.change_player()
+            list_ok = self.grid.verify(self.player)
+            if not list_ok:  # no one can play after that
+                self.end()
+                return
+        self.plot_nb_pawn()
+        self.trace_grille()
+
+    def end(self):
+        nb1, nb2 = self.nb_disks_players()
+        if nb1 == nb2:
+            win_msg = "It's a tie!"
+        else:
+            winner = 1 if nb1 > nb2 else 2
+            win_msg = f"{self.color_player(winner)} wins!"
+        self.plot_nb_pawn(msg=win_msg)
         self.trace_grille()
 
 
-class Ping(tkinter.Frame):
-    """corps principal du programme"""
+class Othello(tkinter.Frame):
+    """Main window and manager of othello game"""
 
     def __init__(self):
         super().__init__()
@@ -234,30 +321,6 @@ class Ping(tkinter.Frame):
         self.jeu.pack(expand=tkinter.YES, fill=tkinter.BOTH, padx=8, pady=8)
 
         self.pack()
-
-    def options(self):
-        """Choice of the number of lines and columns for the grid"""
-        opt = tkinter.Toplevel(self)
-        cur_l = tkinter.Scale(opt, length=200, label="Number of lines :",
-                              orient=tkinter.HORIZONTAL,
-                              from_=1, to=12, command=self.update_nb_rows)
-        cur_l.set(self.jeu.n_row)  # initial position of the cursor
-        cur_l.pack()
-        cur_h = tkinter.Scale(opt, length=200, label="Number of columns :",
-                              orient=tkinter.HORIZONTAL,
-                              from_=1, to=12, command=self.update_nb_cols)
-        cur_h.set(self.jeu.n_col)
-        cur_h.pack()
-
-    def update_nb_cols(self, n):
-        """Updates the number of columns."""
-        self.jeu.n_col = int(n)
-        self.jeu.trace_grille()
-
-    def update_nb_rows(self, n):
-        """Updates the number of rows."""
-        self.jeu.n_row = int(n)
-        self.jeu.trace_grille()
 
     def reset(self):
         """  french!  """
@@ -276,7 +339,7 @@ class Ping(tkinter.Frame):
                              " you have placed and other disks of your colour."
                         ).pack(padx=10, pady=10)
 
-    def by_the_way(self):
+    def about(self):
         """window-message indicating the author and the type of licence"""
         msg = tkinter.Toplevel(self)
         tkinter.Message(msg, width=200, aspect=100, justify=tkinter.CENTER,
@@ -285,4 +348,4 @@ class Ping(tkinter.Frame):
 
 
 if __name__ == '__main__':
-    Ping().mainloop()
+    Othello().mainloop()
